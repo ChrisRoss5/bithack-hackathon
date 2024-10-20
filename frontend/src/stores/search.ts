a22ea5c1c8b385be6a66c5053b5c1f53632a241c
Kristijan Rosandić <kristijan.ros@gmail.com>
2024-10-20 11:57:49 +0200

    [frontend] - fixes

--- frontend/src/stores/search.ts
+++ frontend/src/stores/search.ts
@@ -2,8 +2,8 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 
 export const useSearchStore = defineStore("search", () => {
-  const dateFrom = ref<string>("11/20/2024");
-  const dateTo = ref<string>("11/25/2024");
+  const dateFrom = ref<string>("");
+  const dateTo = ref<string>("");
 
   return {
     dateFrom,
